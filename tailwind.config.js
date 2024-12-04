04171dc64baf92f4aa5b4bd1bb190bbd9ff60a7f
Odd Egil Hegge Selnes <post@odde.org>
2024-12-04 22:23:56 +0100

    created hero section. responsive and beeing rendered by js function

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
   theme: {
     extend: {
+      fontSize: {
+        '2.5xl': '1.75rem',
+        '4.5xl': '2.5rem',
+      },
       colors: {
         background: {
           primary: '#F9F9F9', 
