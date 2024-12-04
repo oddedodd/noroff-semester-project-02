d2c0ad9e3a0f39b9e22941784ffa04ac8785213b
Odd Egil Hegge Selnes <post@odde.org>
2024-12-04 15:07:31 +0100

    functioning responsive navigation in place, but need more work. Will merge and install some tooling before I continue

--- src/main.js
+++ src/main.js
@@ -4,3 +4,16 @@ import router from "./js/router";
 
 await router(window.location.pathname);
 
+/*
+* Mobile menu
+*/
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
+
+  mobileMenuButton.addEventListener('click', function() {
+    mobileMenu.classList.toggle('hidden');
+    menuIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+});
