7df42659910c5237ba51206e4d3b7a147c8d6a30
Odd Egil Hegge Selnes <post@odde.org>
2024-12-14 03:30:32 +0100

    updated home page with call to action below featured listings and fixed bug with mobile navigation where header did not stick

--- src/main.js
+++ src/main.js
@@ -12,11 +12,15 @@ const mobileMenuButton = document.querySelector('#mobile-menu-button');
 const mobileMenu = document.querySelector('#mobile-menu');
 const menuIcon = document.querySelector('#menu-icon');
 const closeIcon = document.querySelector('#close-icon');
+const header = document.querySelector('header');
 
 mobileMenuButton.addEventListener('click', function () {
   mobileMenu.classList.toggle('hidden');
   menuIcon.classList.toggle('hidden');
   closeIcon.classList.toggle('hidden');
+  header.classList.toggle('sticky');
+  header.classList.toggle('top-0');
+  header.classList.toggle('z-50');
 });
 
 document.addEventListener('DOMContentLoaded', () => {
